08290751cca791b4eb2ef30aed3b0cfe55621038
Angeliki Kissani <angelakissani@gmail.com>
2024-09-11 14:24:27 +0300

    receiveTRANSACTIONS

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,12 +1,13 @@
 import express from "express"
 const router =  express.Router();
 
-import { insertTransactions,fetchData,addCategory,fetchTransaction} from "../controllers/transactions.js";
+import { insertTransactions,fetchData,addCategory,fetchTransaction,receiceTransactions} from "../controllers/transactions.js";
 
 router.post("/insert-transactions",insertTransactions);
 router.post("/fetch-data",fetchData);
 router.post("/add-category",addCategory);
 router.post("/fetch-transaction",fetchTransaction);
+router.post("/receive-transactions",receiceTransactions)
 
 
 export default router;
